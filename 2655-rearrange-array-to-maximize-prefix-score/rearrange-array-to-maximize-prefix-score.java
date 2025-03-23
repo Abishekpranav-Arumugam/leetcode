c13495a2fbb560b2a19fc63319e6c87a4456ee02
Abishekpranav-Arumugam <124655810+Abishekpranav-Arumugam@users.noreply.github.com>
2025-03-23 13:52:30 +0530

    Time: 28 ms (55.93%) | Memory: 57.9 MB (33.33%) - LeetSync

--- 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.java
+++ 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.java
@@ -3,25 +3,20 @@ import java.util.Collections;
 
 class Solution {
     public int maxScore(int[] nums) {
-        // Sort the array in descending order
-        Arrays.sort(nums); // Sort in ascending order first
-        int n = nums.length;
-        
-        // Reverse the array to get descending order
-        int[] sortedNums = new int[n];
-        for (int i = 0; i < n; i++) {
-            sortedNums[i] = nums[n - i - 1];
-        }
+        Arrays.sort(nums); 
 
         int count = 0;
         long sum = 0;
-        
-        // Iterate from largest to smallest
+     int[] sortedNums = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            sortedNums[i] = nums[nums.length - i - 1];
+        }
         for (int num : sortedNums) {
             sum += num;
-            if (sum > 0) count++; // Only count if sum remains positive
-            else break; // Stop when sum becomes non-positive
+            if (sum > 0) count++; 
+            else break; 
         }
+
         return count;
     }
 }
