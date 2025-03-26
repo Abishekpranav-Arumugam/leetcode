1aa1da0e26375d4e4ac79282c3b2adb14ccc4081
Abishekpranav-Arumugam <124655810+Abishekpranav-Arumugam@users.noreply.github.com>
2025-03-26 23:31:45 +0530

    Time: 8 ms (78.96%) | Memory: 45.2 MB (70.32%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.java
+++ 923-super-egg-drop/super-egg-drop.java
@@ -1,10 +1,8 @@
 class Solution {
     public int superEggDrop(int k, int n) {
-        // DP Table: dp[m][k] -> max floors we can check with m moves and k eggs
         int[][] dp = new int[n + 1][k + 1];
         int moves = 0;
 
-        // Keep increasing moves until we can check at least `n` floors
         while (dp[moves][k] < n) {
             moves++;
             for (int eggs = 1; eggs <= k; eggs++) {
